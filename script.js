6e33a6e17681bf127b16a447893271d961ee8b5f
Quietstorm10 <156016474+Quietstorm10@users.noreply.github.com>
2024-02-29 21:16:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+  
   // Menu
   const menuToggle = document.querySelector('.menu-toggle');
   const menu = document.querySelector('.menu');
@@ -7,31 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
   menuToggle.addEventListener('click', function() {
     menu.classList.toggle('expanded');
   });
-});
-
-  // Confirming the form
-  const contactForm = document.getElementById('contact-form');
-  const formError = document.getElementById('form-error');
-
-  //Trims whitespaces
-  contactForm.addEventListener('submit', function(event) {
-    const name = document.getElementById('name').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const subject = document.getElementById('subject').value.trim();
-    const message = document.getElementById('message').value.trim();
-
-    //Verifies the form is completed
-    if (!name || !email || !subject || !message) {
-      event.preventDefault();
-      formError.textContent = 'Please fill out all fields.';
-    }
-  });
 
   // Hover changes color
   const h1 = document.querySelector('h1');
 
   h1.addEventListener('mouseenter', function() {
-  // Hover color
+    // Hover color
     h1.style.color = 'gold';
   });
 
